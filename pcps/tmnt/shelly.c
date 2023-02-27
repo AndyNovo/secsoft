a0636dad64d2c4d1b1e067eef39155065d78da10
Andy Novocin <andy@novocin.com>
2023-02-27 14:30:57 +0000

    init for bufferio

--- pcps/tmnt/shelly.c
+++ pcps/tmnt/shelly.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void init(){
+    setvbuf(stdin,NULL,_IONBF,0);
+    setvbuf(stdout,NULL,_IONBF,0);
+    setvbuf(stderr,NULL,_IONBF,0);
+}
+
 int vuln(void){
    char home[512];
    fgets(home, 1024, stdin);
@@ -10,6 +16,8 @@ int vuln(void){
 
 int main(void){
    int x = 23;
+   init();
+   
    printf("Turtle time... %p\n", &x);
    vuln();
    return 0;
